fca58d82cdd266ea903c8cf2eed31fba1e051090
YiHerngOng <ongyi@oregonstate.edu>
2018-10-27 11:28:29 -0700

    finish up kernel perceptron

--- perceptron.py
+++ perceptron.py
@@ -99,6 +99,7 @@ class Perceptron():
 							c = c+1
 				if c > 0:
 					w_avg_dec = (c_s*w_avg_dec+ c*w_dec) / (c_s + c)
+
 				self.acc_valid_avg = []
 				self.acc_train_avg = []
 				accuracy_train = 0
@@ -127,15 +128,49 @@ class Perceptron():
 		for i in xrange(len(self.x_train)):
 			temp = []
 			for j in xrange(len(self.x_train)):
-				temp.append(sign_function(self.x_train[i], self.x_train[j]))
+				temp.append(self.kernel_function(self.x_train[i], self.x_train[j], p))
 			K.append(temp)
 		iteration = 15
-		for k in range(iteration):
-			for a in xrange(len(self.x_train)):
-				u = 
+		# for k in range(iteration):
+		for a in xrange(len(self.x_train)):
+			for b in xrange(self.x_train):
+				u = u + (K[b][0]*alpha[b]*self.y_train[b])
+			if u < 0.0:
+				u_sign = -1
+			elif u >= 0.0:
+				u_sign = 1
+			if self.y_train[a]*u_sign <= 0:
+				alpha[a] = alpha[a] + 1
 
+		self.acc_valid_kern = []
+		self.acc_train_kern = []
 
-		pass
+		y_pred_train = 0
+		accuracy_train = 0
+		accuracy_valid = 0
+		for c in xrange(len(self.x_train)):
+			y_pred_train = 0
+			for d in xrange(len(self.x_train)):
+				y_pred_train = y_pred_train + self.y_train[c]*K[c][d]*alpha[d]
+			y_pred_train_sign = self.sign_function_2(y_pred_train)
+			if y_pred_train_sign == self.y_train[c]:
+				accuracy_train = accuracy_train + 1
+		accuracies_train = float(accuracy_train) / float(len(self.y_train))
+
+		y_pred_valid = 0
+		accuracy_valid = 0
+		accuracy_train = 0
+		for e in xrange(len(self.x_valid)):
+			y_pred_valid = 0
+			for f in xrange(len(self.x_valid)):
+				y_pred_valid = y_pred_valid + self.y_valid[c]*K[c][d]*alpha[d]
+			y_pred_valid_sign = self.sign_function_2(y_pred_valid)
+			if y_pred_valid_sign == self.y_valid[c]:
+				accuracy_valid = accuracy_valid + 1
+		accuracies_valid = float(accuracy_valid) / float(len(self.y_valid))
+		self.acc_valid_kern.append(accuracies_valid)
+		self.acc_train_kern.append(accuracies_train)
+
 
 	# determine whether it's 1 (3) or -1 (5)
 	# prediction function
@@ -146,6 +181,12 @@ class Perceptron():
 			y = 1
 		return y
 
+	def sign_function_2(self, s):
+		if s < 0.0:
+			return -1
+		elif s >= 0.0:
+			return 1
+
 	def kernel_function(self, x_i, x_j, p):
 		return (1 + np.dot(x_i, x_j))**p
 
@@ -154,4 +195,5 @@ if __name__ == '__main__':
 	perceptron = Perceptron('pa2_train.csv', 'pa2_valid.csv')
 	# print perceptron.data
 	# perceptron.online_perceptron()
-	perceptron.average_perceptron()
+	# perceptron.average_perceptron()
+	perceptron.kernel_perceptron(1)
