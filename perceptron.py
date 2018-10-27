b0e93481b7d8810482ab9cefc056eb4936624809
IronGiant <edessale@gmu.edu>
2018-10-26 17:12:21 -0700

    update average perceptron

--- perceptron.py
+++ perceptron.py
@@ -32,8 +32,8 @@ class Perceptron():
 		# pdb.set_trace()
 		# for i in range(iteration):
 		# 	print i
-		self.acc_valid = []
-		self.acc_train = []
+		self.acc_valid_online = []
+		self.acc_train_online = []
 		with open('valid1.csv', 'wb') as csvfile:
 			writer = csv.writer(csvfile, delimiter=',')
 			for i in range(iteration):
@@ -44,34 +44,97 @@ class Perceptron():
 						w_dec = w_dec + (self.y_train[j]*self.x_train[j])
 				accuracy_valid = 0
 				accuracy_train = 0
+
 				# Determine training accuracy
 				for a in xrange(len(self.x_train)):
-					y_pred_train = np.dot(w_dec, self.x_train[a])
-					if y_pred_train < 0.0:
-						y_pred_train = -1
-					elif y_pred_train > 0.0:
-						y_pred_train = 1
+					y_pred_train = self.sign_function(w_dec, self.x_train[a])
 					if y_pred_train == self.y_train[a]:
 						accuracy_train = accuracy_train + 1
-				accuracies_train = float(accuracy_train) / float(len(self.y_train))				
+				accuracies_train = float(accuracy_train) / float(len(self.y_train))		
+
 				# Determine validation accuracy
 				for m, n in enumerate(self.x_valid):
-					y_pred_valid = float(np.dot(w_dec,self.x_valid[m]))
-					if y_pred_valid < 0.0:	# could be 5 
-						y_pred = -1
-					elif y_pred_valid >= 0.0: # could be 3
-						y_pred = 1
+					y_pred_valid = self.sign_function(w_dec, self.x_train[m])
 					if y_pred == self.y_valid[m]:
 						accuracy_valid = accuracy_valid + 1
 						# writer.writerow([y_pred_valid, y_pred, self.y_valid[m]])
 				accuracies_valid = float(accuracy_valid) / float(len(self.y_valid))
 				writer.writerow([accuracies_valid, accuracies_train])
-				self.acc_valid.append(accuracies_valid)
-				self.acc_train.append(accuracies_train)
+				self.acc_valid_online.append(accuracies_valid)
+				self.acc_train_online.append(accuracies_train)
 
 	def average_perceptron(self):
-		pass
+		iteration = 15
+		w = np.zeros(len(self.x_train[0]))
+		w_avg = np.zeros(len(self.x_train[0]))
+		w_dec = []
+		w_avg_dec = []
+		for a in xrange(len(w)):
+			try:
+				w_dec.append(Decimal(w[a]))
+				w_avg_dec.append(Decimal(w_avg[a]))
+			except:
+				raise TypeError
+		w_dec = np.array(w_dec)
+		w_avg_dec = np.array(w_avg_dec)
+
+		c = 0
+		c_s = 0
+		with open('valid2.csv', 'wb') as csvfile:
+			writer = csv.writer(csvfile, delimiter=',')
+			for i in range(iteration):
+				for j in xrange(len(self.x_train)):
+						u = np.dot(w_dec, self.x_train[j])
+						if u < 0.0:
+							u_sign = -1
+						elif u >= 0.0:
+							u_sign = 1
+						if (self.y_train[j]*u_sign) <= 0:
+							if c_s + c > 0:
+								w_avg_dec = (c_s*w_avg_dec + c*w_dec) / (c_s + c)
+							c_s = c_s + c
+							w_dec = w_dec + (self.y_train[j]*self.x_train[j])
+							c = 0
+						else:
+							c = c+1
+				if c > 0:
+					w_avg_dec = (c_s*w_avg_dec+ c*w_dec) / (c_s + c)
+				self.acc_valid_avg = []
+				self.acc_train_avg = []
+				accuracy_train = 0
+				accuracy_valid = 0
+				# Determine training accuracy
+				for a in xrange(len(self.x_train)):
+					y_pred_train = self.sign_function(w_avg_dec, self.x_train[a])
+					if y_pred_train == self.y_train[a]:
+						accuracy_train = accuracy_train + 1
+				accuracies_train = float(accuracy_train) / float(len(self.y_train))
+
+				# Determine validation accuracy
+				for m, n in enumerate(self.x_valid):
+					y_pred_valid = self.sign_function(w_avg_dec, self.x_valid[m])
+					if y_pred_valid == self.y_valid[m]:
+						accuracy_valid = accuracy_valid + 1
+				accuracies_valid = float(accuracy_valid) / float(len(self.y_valid))
+				self.acc_valid_avg.append(accuracies_valid)
+				self.acc_train_avg.append(accuracies_train)
+				writer.writerow([accuracies_valid, accuracies_train])
+
+
+	# determine whether it's 1 (3) or -1 (5)
+	# prediction function
+	def sign_function(self, w, x):
+		if float(np.dot(w, x)) < 0.0:
+			y = -1
+		elif float(np.dot(w, x)) >= 0.0:
+			y = 1
+		return y
+
+
+
+
 if __name__ == '__main__':
 	perceptron = Perceptron('pa2_train.csv', 'pa2_valid.csv')
 	# print perceptron.data
-	perceptron.online_perceptron()
+	# perceptron.online_perceptron()
+	perceptron.average_perceptron()
